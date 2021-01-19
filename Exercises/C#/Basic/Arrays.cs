c028e1370689a334ad19aa073923faaf742baaf8
vitekd55 <vitekdvorak70@gmail.com>
2021-01-19 18:59:07 +0100

    added arrays excersise

---
+++ Exercises/C#/Basic/Arrays.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        public static void Main()
+        {
+            // 1. Vytvoř pole náhodných čísel o velikosti 10
+            // 2. Setřiď pole
+            // 3. Najdi minimum a maximum - nepoužívat funkce
+            // 4. Zjisti průměr
+
+            string text = "Hello, world!";
+
+            // 5. Zjisti počet znaků v text
+            // 6. Nahraď 3. písmeno písmenem c
+            // 7. Najdi první znak druhého slova
+            // 8. Najdi poslední znak textu
+            // 9. Převeď text na malá písmena
+            // 10. Spočítej samohlásky a souhlásky v textu 
+            // 11. Spočítej nejčastější znak
+        }
+    }
+}
